28b4eecebc6216d1a7b71be48a2575a74d62109d
William <liuw6118@gmail.com>
2022-07-02 23:14:39 +1000

    Fourth Commit

--- main.py
+++ main.py
@@ -4,18 +4,39 @@ from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.theming import ThemableBehavior
-from kivymd.uix.list.list import MDList, OneLineIconListItem, IconLeftWidget
-from kivymd.uix.navigationdrawer.navigationdrawer import MDNavigationLayout, MDNavigationDrawerItem
-from kivy.utils import get_color_from_hex
+from kivymd.uix.list.list import MDList, OneLineIconListItem
+from kivy.properties import StringProperty
 from kivy.clock import Clock
-from home import HomeScreen
 
 class ContentNavigationDrawer(BoxLayout):
     pass
 
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+
 class DrawerList(ThemableBehavior, MDList):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        Clock.schedule_once(self.on_start, 0)
+
+    def set_color_item(self, instance_item):
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+    def on_start(self, *args):
+        icons_item = {
+            "folder-home": "Houses",
+            "format-list-bulleted": "Categories",
+            "blur-linear": "Item",
+            "account": "User"
+        }
+        for icon_name in icons_item.keys():
+            self.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
 
 class ScreenManager(ScreenManager):
     def __init__(self, **kwargs):
---
+++ user.py
@@ -0,0 +1,5 @@
+from kivy.uix.screenmanager import Screen
+
+class UserScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
